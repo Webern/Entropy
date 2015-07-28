c33b169d06e9b51390aff0b5788096dd61dd7324
Matthew James Briggs <matthew.james.briggs@gmail.com>
2015-07-28 10:21:20 -0700

    Initial Commit

---
+++ Entropy/Entropy/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+namespace entropy
+{
+    void firstFunction()
+    {
+        std::cout << "Hello Entropy" << std::endl;
+    }
+}
+
+int main(int argc, const char * argv[])
+{
+    entropy::firstFunction();
+    return 0;
+}
+
