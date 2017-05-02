0ff0da3371acb72985b29761b5377530c4754d29
Matthew James Briggs <matthew.james.briggs@gmail.com>
2017-05-01 22:49:56 -0700

    random numbers

--- Sourcecode/Entropy/main.cpp
+++ Sourcecode/Entropy/main.cpp
@@ -3,9 +3,14 @@
 #include "Entropy/Entropy.h"
 #include "Entropy/Config.h"
 #include "mx/api/DocumentManager.h"
+#include "Entropy/RandomDigits.h"
 
 #include <iostream>
 #include <fstream>
+
+#include <stdio.h>      /* printf, scanf, puts, NULL */
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>
 
 int main( int argc, const char* argv[] )
 {
@@ -16,5 +21,7 @@ int main( int argc, const char* argv[] )
     const auto docId = docMgr.createFromScore( scoreData );
     docMgr.writeToFile( docId, config.getOutputFilePath() );
     docMgr.destroyDocument( docId );
+    const auto vec = entropy::getRandomDigits();
+    EN_UNUSED( vec );
     return 0;
 }
