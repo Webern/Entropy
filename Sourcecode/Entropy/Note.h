0e86d42ac7684ee33f969b465111ac2034b3b8db
Matthew James Briggs <matthew.james.briggs@gmail.com>
2017-05-12 08:15:00 -0700

    code file template work

---
+++ Sourcecode/Entropy/Note.h
@@ -0,0 +1,29 @@
+// Copyright (c) Matthew James Briggs
+
+#pragma once
+
+#include "Entropy/Enums.h"
+#include <string>
+
+namespace entropy
+{
+	/// @brief TODO
+	///
+	/// @detailed TODO
+	///
+    class Note
+    {
+    public:
+    	Note();
+
+    public:
+    	void pubicFunc() const;
+
+    private:
+    	int myInt;
+
+    private:
+        void privateFunc();
+        
+    };
+}
